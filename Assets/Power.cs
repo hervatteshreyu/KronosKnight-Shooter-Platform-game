43c9ccdacc936408fa4fc91d02597e0c52531b09
Parth Natu <pnatu@protonmail.com>
2021-04-21 21:15:53 -0400

    cutscenes done

---
+++ Assets/Power.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Power : MonoBehaviour
+{
+    public GameObject impactEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject x = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+        Destroy(x, x.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
+    }
+}
---
+++ Assets/dialog.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class dialog : MonoBehaviour
+{
+
+    public GameObject speech1;
+    public GameObject speech2;
+    public GameObject speech3;
+    public GameObject speech4;
+
+    private void Start()
+    {
+        StartCoroutine("createBubbles");
+    }
+
+    IEnumerator createBubbles()
+    {
+        speech1 = Instantiate(speech1, this.transform);
+        yield return new WaitForSeconds(5);
+        Destroy(speech1);
+        speech2 = Instantiate(speech2, this.transform);
+        yield return new WaitForSeconds(5);
+        Destroy(speech2);
+        speech3 = Instantiate(speech3, this.transform);
+        yield return new WaitForSeconds(5);
+        Destroy(speech3);
+        speech4 = Instantiate(speech4, this.transform);
+        yield return new WaitForSeconds(5);
+        Destroy(speech4);
+        SceneManager.LoadScene("PastScene");
+    }
+}
